ad5b475a511bfabf0ed86d687b29574cd4476b15
pablito Cortes <cjuan1597@gmail.com>
2025-10-27 10:29:41 -0500

    changed in apiClient for prodution

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const NODE_ENV = process.env.NODE_ENV;
+
 export const apiClient = axios.create({
-    baseURL: "http://localhost:5500/api",
+    baseURL: NODE_ENV === 'production' ? 'https://live-chat-back-qjac.onrender.com/api' : 'http://localhost:5500/api',
     withCredentials: true, 
     headers: {
         'Content-Type': 'application/json'
